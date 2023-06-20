ff6ea8cfa43e043b34d44521d2d996aaf9bd4867
SOFTSERVE\arabosh <arabosh@softserveinc.com>
2023-06-20 17:07:31 +0300

    UC#1 Create endpoint

---
+++ TestProject/Controllers/CountriesController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace TestProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CountriesController : ControllerBase
+    {
+        public CountriesController()
+        {
+        }
+
+        [HttpGet(Name = "GetCountries")]
+        public async Task<string> Get()
+        {
+            var httpClient = new HttpClient();
+            using HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "https://restcountries.com/v3.1/all");
+            using HttpResponseMessage response = await httpClient.SendAsync(request);
+            string jsonContent = await response.Content.ReadAsStringAsync();
+            JArray countriesArray = JArray.Parse(jsonContent);
+
+            return countriesArray.ToString();
+        }
+    }
+}
