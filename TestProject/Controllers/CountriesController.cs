df6b6f617359e7c5eca9da39aef22b9b0aa98ba4
SOFTSERVE\arabosh <arabosh@softserveinc.com>
2023-06-20 17:32:18 +0300

    UC#1 Create endpoint

--- TestProject/Controllers/CountriesController.cs
+++ TestProject/Controllers/CountriesController.cs
@@ -12,7 +12,7 @@ namespace TestProject.Controllers
         }
 
         [HttpGet(Name = "GetCountries")]
-        public async Task<string> Get()
+        public async Task<string> Get(string argument1, int argument2, string argument3)
         {
             var httpClient = new HttpClient();
             using HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "https://restcountries.com/v3.1/all");
